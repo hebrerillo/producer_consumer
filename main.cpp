fa65af6d6c7282841c7c5f609814259ba5340fe3
hebrerillo <hebrerillo@gmail.com>
2022-10-04 12:26:58 +0200

    First working example

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@
 #include <string.h>
 #include <atomic>
 
-#define BUFFER_SIZE 10
+#define BUFFER_SIZE 20
+#define SPEED_CONSUMER 400
+#define SPEED_PRODUCER 100
 
 static int buffer[BUFFER_SIZE];
 static std::atomic<bool> quitSignal;
-static size_t currentIndex;
+static int currentIndex;
+static std::mutex mutex;
 
 class Producer
 {
@@ -18,22 +21,54 @@ public:
     {
         while(!quitSignal.load())
         {
-            
+            mutex.lock();
+            if ((currentIndex + 1) < BUFFER_SIZE)
+            {
+                buffer[++currentIndex] = 1;
+                std::cout << "Producing" << std::endl;
+            }
+            mutex.unlock();
+            std::this_thread::sleep_for(std::chrono::milliseconds(SPEED_PRODUCER));
         }
     }
 };
 
 class Consumer 
 {
-
+public:
+    void run()
+    {
+        while(!quitSignal.load())
+        {
+            mutex.lock();
+            if (currentIndex >= 0)
+            {
+                buffer[currentIndex--] = 0;
+                std::cout << "Consuming" << std::endl;
+            }
+            mutex.unlock();
+            std::this_thread::sleep_for(std::chrono::milliseconds(SPEED_CONSUMER));
+        }
+    }
 };
 
 
 int main()
 {
     quitSignal = false;
-    memset(buffer, BUFFER_SIZE, 0);
-    currentIndex = 0;
+    memset(buffer, 0, sizeof(buffer));
+    currentIndex = -1;
+    Consumer consumer;
+    Producer producer;
+
+    std::thread threadProducer(&Producer::run, &producer);
+    std::thread threadConsumer(&Consumer::run, &consumer);
+
+    int input;
+    std::cin >> input;
+    quitSignal.exchange(true);
+    threadProducer.join();
+    threadConsumer.join();
 
     return 0;
 }
