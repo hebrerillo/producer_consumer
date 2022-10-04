5a38b04bda64f66e2d555bbbceb0721742dcbd44
hebrerillo <hebrerillo@gmail.com>
2022-10-04 13:30:15 +0200

    Using condition variables to wait for the other to perform its task

--- main.cpp
+++ main.cpp
@@ -5,15 +5,17 @@
 #include <string.h>
 #include <atomic>
 #include <assert.h>
+#include <condition_variable>
 
 #define BUFFER_SIZE 20
-#define SPEED_CONSUMER 400
+#define SPEED_CONSUMER 100
 #define SPEED_PRODUCER 100
 
 static int buffer[BUFFER_SIZE];
 static std::atomic<bool> quitSignal;
 static int currentIndex;
 static std::mutex mutex;
+static std::condition_variable cv;
 
 class Producer
 {
@@ -22,15 +24,23 @@ public:
     {
         while(!quitSignal.load())
         {
-            mutex.lock();
+            std::unique_lock<std::mutex> lock(mutex);
             if ((currentIndex + 1) < BUFFER_SIZE)
             {
                 ++currentIndex;
                 assert(buffer[currentIndex] == 0);
                 buffer[currentIndex] = 1;
                 std::cout << "Producing" << std::endl;
+                cv.notify_all();
             }
-            mutex.unlock();
+            else
+            {
+                std::cout << "Producer waiting until consumer consumes." << std::endl;
+                cv.wait(lock, [](){
+                    return (currentIndex + 1) < BUFFER_SIZE;
+                });
+            }
+            lock.unlock();
             std::this_thread::sleep_for(std::chrono::milliseconds(SPEED_PRODUCER));
         }
     }
@@ -43,14 +53,22 @@ public:
     {
         while(!quitSignal.load())
         {
-            mutex.lock();
+            std::unique_lock<std::mutex> lock(mutex);
             if (currentIndex >= 0)
             {
                 assert(buffer[currentIndex] == 1);
                 buffer[currentIndex--] = 0;
                 std::cout << "Consuming" << std::endl;
+                cv.notify_all();
             }
-            mutex.unlock();
+            else
+            {
+                std::cout << "Consumer waiting until producer produces." << std::endl;
+                cv.wait(lock, [](){
+                    return (currentIndex >= 0);
+                });
+            }
+            lock.unlock();
             std::this_thread::sleep_for(std::chrono::milliseconds(SPEED_CONSUMER));
         }
     }
