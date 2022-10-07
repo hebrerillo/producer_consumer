2f4d71375b2de10d3fd1c343c98d4f16640f7857
hebrerillo <hebrerillo@gmail.com>
2022-10-07 13:25:58 +0200

    Adding methods to remove and add producers and consumers

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <assert.h>
 #include <condition_variable>
 #include <vector>
+#include <list>
 
 /**
  * Class that represents an item that can be inserted in the shared buffer.
@@ -263,14 +264,50 @@ public:
         addConsumer();
     }
 
+    /**
+     * Adds a producer to produce items into the buffer 'sharedBuffer_'.
+     */
     void addProducer()
     {
         producers_.push_back(new Producer(sharedBuffer_, std::chrono::milliseconds(500)));
     }
 
+    /**
+     * Adds a consumer to consume items from the buffer 'sharedBuffer_'.
+     */
     void addConsumer()
     {
         consumers_.push_back(new Consumer(sharedBuffer_, std::chrono::milliseconds(500)));
+    }
+
+    /**
+     * Removes a consumer.
+     */
+    void removeConsumer()
+    {
+        std::list<Consumer* >::iterator consumer = consumers_.begin();
+        if (consumer == consumers_.end())
+        {
+            return;
+        }
+        (*consumer)->stop();
+        consumers_.erase(consumer);
+        delete (*consumer);
+    }
+
+    /**
+     * Removes a producer.
+     */
+    void removeProducer()
+    {
+        std::list<Producer* >::iterator producer = producers_.begin();
+        if (producer == producers_.end())
+        {
+            return;
+        }
+        (*producer)->stop();
+        producers_.erase(producer);
+        delete (*producer);
     }
 
     ~ProducersConsumersManager()
@@ -289,14 +326,14 @@ public:
     }
 private:
     SharedBuffer* sharedBuffer_;
-    std::vector<Consumer* > consumers_;
-    std::vector<Producer* > producers_;
+    std::list<Consumer* > consumers_;
+    std::list<Producer* > producers_;
 };
 
 int main()
 {
     std::vector<IBufferItem* > buffer;
-    for(size_t i = 0; i < 25; i++)
+    for(size_t i = 0; i < 10; i++)
     {
         buffer.push_back(new BufferItem);
     }
@@ -304,8 +341,26 @@ int main()
     SharedBuffer sharedBuffer(buffer);
     ProducersConsumersManager manager(&sharedBuffer);
 
-    int input;
-    std::cin >> input;
+    int input = -1;
+    while (input != 0)
+    {
+        std::cin >> input;
+        switch(input)
+        {
+            case 1:
+                manager.addProducer();
+                break;
+            case 2:
+                manager.removeProducer();
+                break;
+            case 3:
+                manager.addConsumer();
+                break;
+            case 4:
+                manager.removeConsumer();
+                break;
+        };
+    }
     sharedBuffer.stop();
 
     for(auto itemBuffer: buffer)
